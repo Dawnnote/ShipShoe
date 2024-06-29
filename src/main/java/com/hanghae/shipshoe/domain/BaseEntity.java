f3048ebcf70cbd3ac764b677bcc6b9adb83b959e
lshoon7454@gmail.com <lshoon7454@gmail.com>
2024-06-30 08:57:37 +0900

    test cicd.yaml

--- src/main/java/com/hanghae/shipshoe/domain/BaseEntity.java
+++ src/main/java/com/hanghae/shipshoe/domain/BaseEntity.java
@@ -6,7 +6,6 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -24,9 +23,9 @@ public class BaseEntity {
     @LastModifiedDate
     private LocalDateTime updatedAt;
 
-//    @CreatedBy
-//    @Column(updatable = false)
-//    private String createdBy;
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
 //
 //    @LastModifiedBy
 //    private String updatedBy;
